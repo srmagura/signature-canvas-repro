c656727a4fc21959a8fc6cfb3fdff7ab47babced
Sam Magura <srmagura@gmail.com>
2021-10-23 19:20:10 -0400

    Add Example3

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import { Example1 } from "./Screens/Example1";
 import { Example2 } from "./Screens/Example2";
+import { Example3 } from "./Screens/Example3";
 
 const Tab = createBottomTabNavigator();
 
@@ -12,6 +13,7 @@ export default function App() {
       <Tab.Navigator>
         <Tab.Screen name="Example1" component={Example1} />
         <Tab.Screen name="Example2" component={Example2} />
+        <Tab.Screen name="Example3" component={Example3} />
       </Tab.Navigator>
     </NavigationContainer>
   );
--- Screens/Example3.tsx
+++ Screens/Example3.tsx
@@ -2,18 +2,16 @@ import React from "react";
 import { View, Text } from "react-native";
 import SignatureScreen from "react-native-signature-canvas";
 
-export function Example2(): React.ReactElement {
+export function Example3(): React.ReactElement {
   return (
     <View style={{ height: "100%", flex: 1, backgroundColor: "paleturquoise" }}>
       <Text style={{ padding: 4 }}>
-        There are several issues when rotated=true: (1) the confirm and clear
-        buttons are not visible, (2) the signature canvas only takes up part of
-        the webview, and (3) scrolling horizontally moves the signature canvas
-        vertically. The behavior becomes even weirder if you switch your phone
-        to landscape mode.
+        Please view this example in landscape mode. There are a number of
+        issues: (1) the signature canvas only takes up the bottom half of the
+        screen, (2) there are vertical and horizontal scrollbars but you can't
+        actually scroll, and (3) the confirm and clear buttons are not visible.
       </Text>
       <SignatureScreen
-        rotated
         descriptionText="Please sign above"
         confirmText="Confirm"
         clearText="Clear"
